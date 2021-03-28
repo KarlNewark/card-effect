1501b0a586c1c8fb02e8ca05ade56b68d2afa3d3
KarlNewark <karlnewark@Karls-MBP.broadband>
2021-03-28 00:59:01 +0000

    animation

--- public/index.js
+++ public/index.js
@@ -1,26 +1,43 @@
-console.log('hello world')
-
-setTimeout(()=> {
-
-if ('DeviceOrientationEvent' in window) {
-    window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+if ("DeviceOrientationEvent" in window) {
+    window.addEventListener("deviceorientation", deviceOrientationHandler, false);
   } else {
-    document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
+    console.log("Device Orientation API not supported.");
   }
   
-  function deviceOrientationHandler (eventData) {
+  var lastDegrees = {
+    fb: 0,
+    lr: 0
+  };
+  
+  function deviceOrientationHandler(eventData) {
     var tiltLR = eventData.gamma;
     var tiltFB = eventData.beta;
     var dir = eventData.alpha;
-    
-    document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  
+    if (
+      tiltLR - lastDegrees.lr > 7.5 ||
+      tiltLR - lastDegrees.lr < -7.5 ||
+      tiltFB - lastDegrees.fb > 7.5 ||
+      tiltFB - lastDegrees.fb < -7.5
+    ) {
+      lastDegrees.lr = tiltLR;
+      lastDegrees.fb = tiltFB;
+      var dots = document.querySelector(".dots");
+  
+      dots.style.opacity = `1`;
+      setTimeout(() => {
+        dots.style.opacity = `0`;
+      }, 1000);
+    }
+  
+    document.getElementById("doTiltLR").innerHTML = Math.round(
+      tiltLR - lastDegrees.lr
+    );
     document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
     document.getElementById("doDirection").innerHTML = Math.round(dir);
   
-    var logo = document.getElementById("imgLogo");
-    logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
-    logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
-    logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+    var holo = document.querySelector(".mask");
+  
+    holo.style.backgroundPosition = `${tiltLR}% ${tiltFB}%`;
   }
-
-}, 5000);
+  
